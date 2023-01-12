cc30e15363100cccbe0901f638671d09e6a3aa66
Amos Maina <amossmwangii@gmail.com>
2023-01-12 02:43:49 -0800

    changes in all the files

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * check_cycle - a function that checks if a single linked list has a cycle
+ * @list: a singly linked list
+ *
+ * Return: 0 if there is no cycle 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *turtle, *hare;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	turtle = list->next;
+	hare = list->next;
+
+	while (turtle && hare && hare->next)
+	{
+		if (turtle == hare)
+			return (1);
+
+		turtle = turtle->next;
+		hare = hare->next;
+	}
+
+	return (0);
+}
---
+++ 0x00-python-hello_world/README.md
@@ -0,0 +1,2 @@
+This directory contains various tasks on the implementation of a higher programing language.
+ The various tasks include various real world problems introducting various concepts.
