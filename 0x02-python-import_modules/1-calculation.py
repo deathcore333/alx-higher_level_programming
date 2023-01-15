32a6b873de347ae3c2a138d9118dcaad2d122dd3
Amos Maina <amossmwangii@gmail.com>
2023-01-15 06:56:45 -0800

    added the  correct syntax

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-if __name__ = "__main__":
+if __name__ == "__main__":
     """Print the sum, difference, multiple and quotient of 10 and 5"""
-    from calculator_1.py import add, sub, mul, div
+    from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-if __name__ = "__main__":
+if __name__ == "__main__":
     """Print the number of arguments and the arguments"""
     import sys
-
+    
     count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
