b10cf572700f9bd3d8619e4a10281c92f3c4d97f
Amos Maina <amossmwangii@gmail.com>
2023-01-15 07:06:22 -0800

    new changes on line 15

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
         sys.exit(1)
 
     a = int(sys.argv[1])
-    b = int(sys.argv[1])
+    b = int(sys.argv[3])
     print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
