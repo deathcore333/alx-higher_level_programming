f1db269a3ad6c30ca043c7f539739f1b5916a380
Amos Maina <amossmwangii@gmail.com>
2023-01-29 02:16:07 -0800

    a function that doubles all the values in a dictionary

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for key in a_dictionary:
-        a_dictionary[key] = a_dictionary[key] * 2
-    return a_dictionary
+    new_dic = a_dictionary.copy()
+
+    for key in new_dic:
+        new_dic[key] = new_dic[key] * 2
+    return new_dic
