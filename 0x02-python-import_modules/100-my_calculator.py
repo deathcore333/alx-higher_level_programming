bba93c7e54123b728af687d9fbeba43cdd7ab1f6
Amos Maina <amossmwangii@gmail.com>
2023-01-29 15:05:28 -0800

    fixed calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 
     ops = {"+": add, "-": sub, "*": mul, "/": div}
     if sys.argv[2] not in list(ops.keys()):
-        print("Unknown operator. Available operators: +, -, *, /")
+        print("Unknown operator. Available operators: +, -, *, and /")
         sys.exit(1)
 
     a = int(sys.argv[1])
