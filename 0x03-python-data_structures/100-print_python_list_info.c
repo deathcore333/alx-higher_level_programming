67d3e47626f88a4573af6a044d30ca6f56a2e513
Amos Mwangi Maina <100504477+deathcore333@users.noreply.github.com>
2023-01-29 23:47:30 +0000

    a few additions

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -10,15 +10,15 @@
 
 void print_python_list_info(PyObject *p)
 {
-	int i;
-	long int size = PyList_Size(p);
-	PyListObject *obj = (PyListObject *)p
+    int i;
+    long int size = PyList_Size(p);
+    PyListObject *obj = (PyListObject *)p;
 
-	printf("[*] Size of Python List = %li\n", size);
-	printf("[*] Allocated = %li\n", obj->allocated);
+    printf("[*] Size of Python List = %li\n", size);
+    printf("[*] Allocated = %li\n", obj->allocated);
 
-	for (i = 0; i < PyList_size(p); i++)
-	{
-	printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
-	}
+    for (i = 0; i < size; i++)
+    {
+    printf("Element %i: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+    }
 }
