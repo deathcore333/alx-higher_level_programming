192daa8562b6174abf0a7cc995131661a04c2557
Amos Maina <amossmwangii@gmail.com>
2023-02-04 04:25:19 -0800

    a function that creates a  class with an object size and printes the area of the square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
+
+    def my_print(self):
+        if size = 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            return TypeError("size must be an integer")
+        if value < 0:
+            return ValueError("size must be >= 0")
+        self.__size = value
