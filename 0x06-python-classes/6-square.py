a65725eb5b208a7747f8bdaf981291e6d9361158
Amos Maina <amossmwangii@gmail.com>
2023-02-04 08:29:27 -0800

    New changes attribute error corrected'

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,13 +6,14 @@
 class Square:
     """Represent a square."""
 
-    def __init__(self, size, position):
+    def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
         Args:
             size (int): The size of the new square.
+            position (int, int): position of new square
         """
-        self.__size = size
-        self.__postion = position
+        self.size = size
+        self.postion = position
 
     @property
     def size(self):
@@ -39,6 +40,7 @@ class Square:
                 not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         """Return the area of the current square"""
@@ -49,6 +51,7 @@ class Square:
         if self.__size == 0:
             print()
             return
+
         [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
