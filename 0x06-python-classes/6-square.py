62732e5c650c08009da8940d6d5718a0704ab975
Amos Maina <amossmwangii@gmail.com>
2023-02-04 09:01:23 -0800

    sometimes the return statement is necesary

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -50,6 +50,7 @@ class Square:
         """Print the area with the # character."""
         if self.__size == 0:
             print()
+            return
 
         [print("") for i in range(0, self.position[1])]
         for i in range(0, self.__size):
--- 0x06-python-classes/main_6.py
+++ 0x06-python-classes/main_6.py
@@ -2,8 +2,5 @@
 
 Square = __import__('6-square').Square
 
-my_square = Square(3, (1, 1))
-print(my_square.size)
-print(my_square.area())
-print(my_square.position)
-
+my_square = Square(0, (10, 3))
+my_square.my_print()
