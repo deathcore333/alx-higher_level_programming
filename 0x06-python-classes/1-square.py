ad0c6d47034f1585f5b6b60ea4b7e1eea70b6fd2
Amos Maina <amossmwangii@gmail.com>
2023-02-04 04:41:39 -0800

    added comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 
+"""Define a class Square"""
+
+
 class Square:
+    """represent a square"""
+    
     def __init__(self, size):
+        """initiate a new Square.
+        Args:
+            size (int): The size of the new square
+        """
         self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 
+"""Define a class Square"""
+
+
 class Square:
+    """Represent a square."""
     def __init__(self, size=0):
+        """Initialize a new square.
+        Args:
+            size (0): size of the new square
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
