110fbae460ecd219949aa69cd19faa76da817344
Amos Maina <amossmwangii@gmail.com>
2023-02-07 11:11:12 -0800

    a program that returns the area and the perimeter of a rectangle

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+"""Defines a class Rectangle"""
+
+
+class Rectangle:
+    """represents an empty class Rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initializes the class with objects
+        Args:
+            width (int): represents the width
+            height (int): represents the height
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieves the value of width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """retrieve the value of height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """"Returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Returns the rectangle's perimeter"""
+        return (2 * (self.__width + self.__height))
