c801cea6b3397e19c550df179099ef8b675d1cfc
Amos Maina <amossmwangii@gmail.com>
2023-02-07 13:59:50 -0800

    included a new condition

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -47,4 +47,6 @@ class Rectangle:
 
     def perimeter(self):
         """Returns the rectangle's perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return (2 * (self.__width + self.__height))
