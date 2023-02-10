7610dd48cc29dc4fbba4ad27a5e414d4e6c35713
Amos Maina <amossmwangii@gmail.com>
2023-02-10 09:48:37 -0800

    syntax corrections

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -4,6 +4,9 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """Represents a rectangle using BaseGeometry"""
+
+    def __init__(self, width, height)
     """Initialize a new Rectangle
     Args:
         width (int): width of the rectangle
