bd738c4ce3ea364147d2864637bd10353f99b2d6
Amos Maina <amossmwangii@gmail.com>
2023-02-10 08:03:37 -0800

    Defines a class Rectangle that inherits from BaseGeometry

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines a class rectangle that inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Initialize a new Rectangle
+    Args:
+        width (int): width of the rectangle
+        height (int): height of the new Rectangle
+    """
+    self.integer_validator("width", width)
+    self.__width = width
+    self.integer_validator("height", height)
+    self.__height = heighti
+
+    def area(self):
+        """computes the area of a Rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return the print() and str() representation of a rectangle"""
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
