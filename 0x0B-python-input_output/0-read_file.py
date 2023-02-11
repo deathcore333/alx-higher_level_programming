9daa6a23ea8dc569b89d61851bb650503e5b0ac6
Amos Maina <amossmwangii@gmail.com>
2023-02-11 03:03:02 -0800

    correction

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,5 +10,5 @@ def read_file(filename=""):
     Return:
         Nothing
     """
-    with open('filename', r, encoding='UTF-8') as f:
+    with open(filename, 'r', encoding='UTF-8') as f:
         print(f.read(), end="")
