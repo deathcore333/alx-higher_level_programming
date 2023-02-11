645753f1bb216cff0033556956728a8dc4bb5bf7
Amos Maina <amossmwangii@gmail.com>
2023-02-11 03:05:38 -0800

    append is not a functioncalll

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -12,4 +12,4 @@ def append_write(filename="", text=""):
         Number of characters added
     """
     with open(filename, 'a', encoding='utf-8') as f:
-        return f.append(text)
+        return f.write(text)
