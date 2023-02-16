5475cb7d22a734e18b008c0ed8c76fd7868c6efe
Amos Maina <amossmwangii@gmail.com>
2023-02-15 19:49:19 -0800

    made file executable

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
---script that creates the database hbtn_0c in your MYSQL server.
-CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
+--is cript that creates the database hbtn_0c in your MYSQL server.
+CREATE DATAB IF NOT EXISTS `hbtn_0c_0`;
