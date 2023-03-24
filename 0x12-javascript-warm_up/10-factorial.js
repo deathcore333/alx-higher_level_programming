fbbbee3840081378536013d43b15634dc4337362
deathcore333 <amossmwangii@gmail.com>
2023-03-24 18:28:36 +0300

    corrections on style

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
-function factorial(num) {
-if (isNaN(num)) {
-  console.log('1');
-} else {
-  let factorial = 1;
-  for (let i = 2; i <= num; i++) {
-    factorial *= i;
+function factorial (num) {
+  if (isNaN(num)) {
+    console.log('1');
+  } else {
+    let factorial = 1;
+    for (let i = 2; i <= num; i++) {
+      factorial *= i;
+    }
+    return factorial;
   }
-	return factorial
-}
 }
 
 const numf = parseInt(process.argv[2]);
 
-  console.log(factorial(numf));
-
+console.log(factorial(numf));
