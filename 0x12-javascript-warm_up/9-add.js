099a175d32b0b60000f2910e888d8756515513fc
deathcore333 <amossmwangii@gmail.com>
2023-03-24 13:41:25 +0300

    fixed semistandard

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,17 +1,14 @@
 #!/usr/bin/node
 
-
-function add(a, b) {
-if (isNaN(a) || isNaN(b)) {
-        return 'NaN';
-} else {
-        return a + b;
+function add (a, b) {
+  if (isNaN(a) || isNaN(b)) {
+    return 'NaN';
+  } else {
+    return a + b;
+  }
 }
-}
-
 
 const num1 = parseInt(process.argv[2]);
 const num2 = parseInt(process.argv[3]);
-
 
 console.log(add(num1, num2));
