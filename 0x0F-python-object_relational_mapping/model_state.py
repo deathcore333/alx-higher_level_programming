4e30089263bbb91499ef6916cb1f0e20a3f3c6aa
deathcore333 <amossmwangii@gmail.com>
2023-04-03 02:14:52 +0300

    module documentation added

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,6 +7,7 @@ from sys import argv
 from model_state import Base, State
 
 if __name__ == "__main__":
+    """ module not executed on import """
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
                 argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
