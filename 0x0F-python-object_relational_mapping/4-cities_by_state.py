6c8310c9d08712d25143a35e468b47bd8383e173
deathcore333 <amossmwangii@gmail.com>
2023-04-03 01:29:31 +0300

    a new script

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+scripts that lists all the cities from the database
+"""
+
+import MySQL
+from sys import argv
+
+if __name__ == "__main__":
+    """ not execute when imported """
+
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    server = 'localhost'
+    port 3306
+
+    conn = MySQL.connect(host=server,
+                         port=port,
+                         user=username,
+                         passwd=password,
+                         db=database
+                         )
+
+    cur = conn.cursor()
+    sql = """
+          SELECT
+                cities.id, cities.name, states.name
+          FROM
+                cities
+          INNER JOIN
+                states ON
+                    cities.state_id=states.id"""
+    cur.execute(sql)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(raw)
+    cur.close()
+    conn.close()
