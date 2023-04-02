662a444c23302943a0e998b96c10f1c6acad97e7
deathcore333 <amossmwangii@gmail.com>
2023-04-03 02:32:04 +0300

    corrected the whole logic

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,11 +3,21 @@
 Start link class to table in database
 """
 from sqlalchemy import create_engine
-from sys import argv
-from model_state import Base, State
+import sys
+from sqlalchemy.ext.declarative import declarative_base
 
-if __name__ == "__main__":
-    """ module not executed on import """
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-                argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+Base = declarative_base
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                        sys.argv[1], sys.argv[2], sys.argv[3]),
+                       pool_pre_ping=True)
+
+
+class State(Base):
+    """ Defines a class State that inherits from Base """
+    __tablename__ = 'states'
+    id = column(integer, primary_key=True, autoincrement=True)
+    name = column(string(128), nullable=False)
+
+
+Base.metadata.create_all(engine)
