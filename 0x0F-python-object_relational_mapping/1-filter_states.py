06f19fcd7ff95ccc0e28c5345ef8523ec1c0a869
deathcore333 <amossmwangii@gmail.com>
2023-04-03 00:47:53 +0300

    corrected syntax error

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                            passwd=argv[2],
                            db=argv[3])
     cur = conn.cursor()
-    cur.execute("""SELECT * FROM states WHERE name LIKEBINARY 'N%'
+    cur.execute("""SELECT * FROM states WHERE name LIKE BINARY 'N%'
                 ORDER BY states.id ASC""")
     query_rows = cur.fetchall()
     for row in query_rows:
