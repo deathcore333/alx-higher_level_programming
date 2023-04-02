5f17621e7f52a5980613fad71543ff179de90be4
deathcore333 <amossmwangii@gmail.com>
2023-04-03 02:42:07 +0300

    did my best

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 """
-This module defines a State class that represents a table in a MySQL database, with columns for an auto-generated,
-unique integer ID and a string name with a maximum length of 128 characters. The class inherits from SQLAlchemy's
-declarative_base() function and is linked to the 'states' table in the database.
-
-The module requires the following dependencies:
-    - SQLAlchemy
-
-Example usage:
-    python3 my_module.py <username> <password> <database_name>
+Contains the class definition of State an instance of
+Base = declarative_base()
 """
+
 from sqlalchemy import create_engine, Integer, column, string
 import sys
 from sqlalchemy.ext.declarative import declarative_base
@@ -26,9 +20,9 @@ class State(Base):
     Represents a State object that is linked to a MySQL database table.
 
     Attributes:
-        __tablename__ (str): The name of the database table that the class maps to.
-        id (int): An auto-generated, unique integer ID for the State object. This is a primary key and can't be null.
-        name (str): A string representing the name of the State. It has a maximum length of 128 characters and can't be null.
+        __tablename__ (str): database name.
+        id (int): An auto-generated, unique integer
+        name (str): string representing state name
     """
     __tablename__ = 'states'
     id = column(integer, primary_key=True, autoincrement=True)
