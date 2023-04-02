ef285a2bea618c4dbf7e06fd797e62fbbbcda92e
deathcore333 <amossmwangii@gmail.com>
2023-04-03 00:43:52 +0300

    a script that print all states with a name starting with N (Uppercase)

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Lists all states with a name starting with N(uppercase)
+"""
+
+if __name__ == "__main__":
+    """ not be executed when imported """
+    import MySQLdb
+    from sys import argv
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=argv[1],
+                           passwd=argv[2],
+                           db=argv[3])
+    cur = conn.cursor()
+    cur.execute("""SELECT * FROM states WHERE name LIKEBINARY 'N%'
+                ORDER BY states.id ASC""")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
