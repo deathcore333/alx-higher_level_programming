1c626a8eea14ff09c945130844be1c76fdd59bc2
deathcore333 <amossmwangii@gmail.com>
2023-04-03 02:46:39 +0300

    changed

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,32 +1,13 @@
 #!/usr/bin/python3
-"""
-Contains the class definition of State an instance of
-Base = declarative_base()
+"""Start link class to table in database
 """
 
-from sqlalchemy import create_engine, Integer, column, string
 import sys
-from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
+from sqlalchemy import (create_engine)
 
-Base = declarative_base
-
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-                        sys.argv[1], sys.argv[2], sys.argv[3]),
-                       pool_pre_ping=True)
-
-
-class State(Base):
-     """
-    Represents a State object that is linked to a MySQL database table.
-
-    Attributes:
-        __tablename__ (str): database name.
-        id (int): An auto-generated, unique integer
-        name (str): string representing state name
-    """
-    __tablename__ = 'states'
-    id = column(integer, primary_key=True, autoincrement=True)
-    name = column(string(128), nullable=False)
-
-
-Base.metadata.create_all(engine)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
