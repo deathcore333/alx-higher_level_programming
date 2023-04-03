f8e141b3d74505aa5ba21ca23ee3bd2a5d8a82ce
deathcore333 <amossmwangii@gmail.com>
2023-04-03 17:31:48 +0300

    trying something else

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -5,20 +5,14 @@ Defines a class City that inherits from base, similar to State
 
 from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base
 
+Base = declarative_base()
 
 class City(Base):
     """ City class """
     __tablename__ = 'cities'
 
-    id = Column(Integer,
-                autoincrement=True,
-                nullable=False,
-                unique=True,
-                primary_key=True
-                )
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer,
-                      ForeignKey("states.id"),
-                      nullable=False
-                      )
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -4,7 +4,8 @@ Defines a State model.
 Inherits from SQLAlchemy Base and links to the MySQL table states.
 """
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship, backref, declarative_base
+from sqlalchemy.orm import relationship, declarative_base
+from relationship_city import Base, City
 
 Base = declarative_base()
 
@@ -17,8 +18,7 @@ class State(Base):
     name (sqlalchemy.String): The state's name.
     """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, unique=True,
-                nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     cities = relationship("City",
                           backref="state",
