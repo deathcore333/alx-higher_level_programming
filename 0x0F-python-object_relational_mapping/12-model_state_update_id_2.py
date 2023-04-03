6d560915e0b454adf84edce0c1781d9e10164c3f
deathcore333 <amossmwangii@gmail.com>
2023-04-03 13:57:05 +0300

    a script that changes the name of the second state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Script that changes the name of a State object from the database
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_update = session.query(State).filter(
+                    State.id == 2).update({"name": "New Mexico"})
+    session.commit()
+    session.close()
