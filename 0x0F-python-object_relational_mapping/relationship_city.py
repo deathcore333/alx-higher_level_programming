d76d841bca6dcbcfc623c2aecbcf4d342f47fae7
deathcore333 <amossmwangii@gmail.com>
2023-04-03 17:40:10 +0300

    good I hate it fuck this bullshit

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -14,6 +14,6 @@ class City(Base):
     """ City class """
     __tablename__ = 'cities'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
