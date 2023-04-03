c9e990082389e0a99b3d8ead13a549e690ba0f94
deathcore333 <amossmwangii@gmail.com>
2023-04-03 03:19:49 +0300

    added committment

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-# Defines a State model.
-# Inherits from SQLAlchemy Base and links to the MySQL table states.
-
+"""
+Defines a State model.
+Inherits from SQLAlchemy Base and links to the MySQL table states.
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base
 
@@ -10,7 +11,8 @@ Base = declarative_base()
 
 class State(Base):
     """Represents a state for a MySQL database.
-    __tablename__ (str): The name of the MySQL table to store States.
+
+   __tablename__ (str): The name of the MySQL table to store States.
     id (sqlalchemy.Integer): The state's id.
     name (sqlalchemy.String): The state's name.
     """
