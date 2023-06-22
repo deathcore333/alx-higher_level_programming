ebb6c74fbd2efe7bc23019a46a40aa3329802ad4
deathcore333 <amossmwangii@gmail.com>
2023-06-23 02:57:20 +0300

    more changes

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-const inputs = process.argv.length;
-
-if (inputs < 3) {
-  console.log('No argument');
-} else {
-  console.log(process.argv[2]);
-}
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
