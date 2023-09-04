8d6f2177dd718ea3e1f89d4bc43f8291c9e3146b
deathcore333 <amossmwangii@gmail.com>
2023-09-04 05:40:01 +0300

    corrected response

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -11,6 +11,6 @@
 import requests
 
 if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status')
+    r = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:\n\t- type: {}\n\t- content: {}"
           .format(type(r.text), r.text))
